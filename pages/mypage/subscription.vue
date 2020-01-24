e9132649629d83248d82c56fde10ac6adefb5550
takonasu <piano88_viola_tako8@hotmail.co.jp>
2020-01-24 15:24:48 +0900

    add: サブスク登録状況一覧

---
+++ pages/mypage/subscription.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="section">
+    <div class="is-mobile">
+      <div v-for="item in history">
+        <b-message title="item.plan" aria-close-label="Close message" style="margin-bottom:10px;">
+          <ul>
+            <li>ID:{{ item.subscription_id }}</li>
+            <li>プラン:{{ item.plan[0].name }}</li>
+            <li>状態：{{ item.status }} </li>
+            <li>登録日：{{ item.start_at }}</li>
+          </ul>
+        </b-message>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'HomePage',
+
+  components: {
+
+  },
+
+  data () {
+    return {
+      history: null
+    }
+  },
+  mounted () {
+    this.$axios.$get('https://dev.api.twinte.net/v1/payment/subscriptions', {
+      withCredentials: true
+    })
+      .then(response => (this.history = response))
+  }
+}
+</script>
