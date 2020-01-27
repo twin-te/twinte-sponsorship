2c459eb16a1c3bc9cf36e0e2939dbc00c9d9cccf
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-01-27 12:29:31 +0900

    add: 雑にフォームを設置

---
+++ pages/mypage/patch.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-field label="Name">
+      <b-input v-model="name" placeholder="NAME" />
+    </b-field>
+    <b-field label="link">
+      <b-input placeholder="URL" type="url" />
+    </b-field>
+    <b-button @click="clickMe">
+      更新
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
