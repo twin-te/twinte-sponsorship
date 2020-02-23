32627783314ec47199b052737441835573c4aa35
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-02-23 14:13:34 +0900

    add: sweetalert, fix: login url

--- components/LoginAlert.vue
+++ components/LoginAlert.vue
@@ -8,7 +8,7 @@
       </header>
       <section class="modal-card-body">
         <div>
-          <a href="https://dev.api.twinte.net/v1/auth/google?redirect-to=https://www.twinte.net">
+          <a href="https://api.twinte.net/v1/auth/google?redirect-to=https://sponsorship.twinte.net">
             <img
               src="~assets/authing/sign-in-with-google.png"
               alt="sign in with google"
@@ -16,7 +16,7 @@
           </a>
         </div>
         <div>
-          <a href="https://dev.api.twinte.net/v1/auth/apple?redirect-to=https://www.twinte.net">
+          <a href="https://api.twinte.net/v1/auth/apple?redirect-to=https://sponsorship.twinte.net">
             <img
               src="~assets/authing/sign-in-with-apple.png"
               alt="SignInWithApple"
@@ -24,7 +24,7 @@
           </a>
         </div>
         <div>
-          <a href="https://dev.api.twinte.net/v1/auth/twitter?redirect-to=https://www.twinte.net">
+          <a href="https://api.twinte.net/v1/auth/twitter?redirect-to=https://sponsorship.twinte.net">
             <img
               src="~assets/authing/sign-in-with-twitter.png"
               alt="ign-in-with-twitter"
