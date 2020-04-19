d648470d266a70b22da8361540c2f46c4874cf9b
takonasu <piano88_viola_tako8@hotmail.co.jp>
2020-04-19 12:11:56 +0900

    add: 寄付完了時に金額を表示

--- pages/success.vue
+++ pages/success.vue
@@ -1,9 +1,22 @@
 <template>
   <section class="section">
     <h1 class="title">
-      ありがとうございました。
+      ありがとうございました!
     </h1>
+    <h2 class="kind">
+      <nav v-if="$route.query.type === 'onetime'">
+      1回きりの寄付
+      </nav>
+      <nav v-else-if="$route.query.type === 'subscription'">
+      サブスクリプション寄付（継続寄付）
+      </nav>
+    </h2>
+    <p class="has-text-centered has-text-primary price">
+      ¥{{ $route.query.amount }}
+    </p>
     <p>
+      以上の金額が寄付されました。
+      <br>
       <nuxt-link to="/mypage">
         マイページ
       </nuxt-link>からユーザー情報の編集や寄付の履歴が確認できます。
@@ -15,3 +28,16 @@
 export default {
 }
 </script>
+
+<style lang="scss" scoped>
+.kind{
+  font-style: normal;
+  font-weight: bold;
+}
+.price{
+  font-style: normal;
+  font-weight: bold;
+  font-size: 50px;
+  line-height: 72px;
+}
+</style>
