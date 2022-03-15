7ca75086588e59fbc9e76e7ef6966be0f77c60e5
takonasu <piano88_viola_tako8@hotmail.co.jp>
2022-03-16 01:13:18 +0900

    add: package version

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,18 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+  overrides: [
+    {
+      files: ["*.vue"],
+      processor: "vue/.vue",
+    },
   ],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  extends: ["@nuxtjs", "plugin:nuxt/recommended"],
   // add your custom rules here
-  rules: {
-  }
-}
+  rules: {},
+};
