96cb19a27a89aef1e60ebd3baaae4f0fba2649e4
Suzuki Kensuke <s1811349@s.tsukuba.ac.jp>
2022-04-16 13:16:21 +0900

    add drawer open button

--- components/MobileHeader.tsx
+++ components/MobileHeader.tsx
@@ -1,12 +1,34 @@
 import styles from '../styles/components/MobileHeader.module.scss';
 import Link from 'next/link';
+import Drawer from 'react-modern-drawer';
+import 'react-modern-drawer/dist/index.css';
+import { useState } from 'react';
+import Sidebar from './Sidebar';
 
 const MobileHeader: React.FC = () => {
+	const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+	const toggleDrawer = () => {
+		setIsDrawerOpen((prevState) => !prevState);
+	};
+
 	return (
-		<header className={styles.header}>
-			<Link href="/" passHref>
-				<img src="/images/twinte-sponsor-title.png" alt="Twin:te_Logo" />
-			</Link>
+		<header>
+			<div className={styles.navbar}>
+				<button onClick={toggleDrawer} className={`navbar-burger ${styles.navbarBurger}`}>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+				</button>
+				<div className="navbar-brand">
+					<Link href="/" passHref>
+						<img src="/images/twinte-sponsor-title.png" alt="Twin:te_Logo" />
+					</Link>
+				</div>
+			</div>
+
+			<Drawer open={isDrawerOpen} onClose={toggleDrawer} direction="left">
+				<Sidebar />
+			</Drawer>
 		</header>
 	);
 };
