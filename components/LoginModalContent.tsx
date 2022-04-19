19eeb9cfcb0e97c3c1eb24900a18136ae1d634e0
Suzuki Kensuke <s1811349@s.tsukuba.ac.jp>
2022-04-19 10:37:04 +0900

    add margin

--- components/LoginModalContent.tsx
+++ components/LoginModalContent.tsx
@@ -8,17 +8,17 @@ const LoginModalContent = (
 	<>
 		<p>Twin:teアプリと同じアカウントでログインすることができます。</p>
 		<div className="has-text-centered">
-			<div>
+			<div className="m-3">
 				<a href={getLoginUrl('apple')}>
 					<Image src={AppleLogin} alt="sign in with apple" />
 				</a>
 			</div>
-			<div>
+			<div className="m-3">
 				<a href={getLoginUrl('twitter')}>
 					<Image src={TwitterLogin} alt="sign in with apple" />
 				</a>
 			</div>
-			<div>
+			<div className="m-3">
 				<a href={getLoginUrl('google')}>
 					<Image src={GoogleLogin} alt="sign in with apple" />
 				</a>
