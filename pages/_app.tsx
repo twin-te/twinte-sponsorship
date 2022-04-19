c4328b35352b892f22daae2cae1e9f74c63d1ece
Suzuki Kensuke <s1811349@s.tsukuba.ac.jp>
2022-04-19 14:56:53 +0900

    add default seo settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,15 @@ import Layout from '../components/Layout';
 import '@stripe/stripe-js';
 import * as dayjs from 'dayjs';
 import 'dayjs/locale/ja';
+import { DefaultSeo } from 'next-seo';
+import SEO from '../next-seo.config';
 
 dayjs.locale('ja');
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<Layout>
+			<DefaultSeo {...SEO} />
 			<Component {...pageProps} />
 		</Layout>
 	);
